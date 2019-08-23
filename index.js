0bf8f9affa9e159b63a66f057597659ab5d90830
Kang Taehoon <jadetypehoon@gmail.com>
2019-08-23 21:25:10 +0900

    test ready

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const app = express();
 // app.use(cors());
 
 
-
-app.use('/',(req, res)=> {
+app.use('/', (req, res) => {
   res.send("this is es2 server deployed");
 });
 
-app.listen( 5000, ()=> {
+app.use('/useful', (req, res) => {
+  data = {
+    seoul: "CodeStates",
+    busan: "CodeBoots",
+    Daegu: "CodeJJambbong",
+    Gwanju: "CodeJJajang"
+  };
+  res.send(JSON.stringify(data));
+});
+
+app.listen(5000, () => {
   console.log("5000_ start!");
 });
